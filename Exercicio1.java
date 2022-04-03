54c357c2d98c1a99943283d7736c917d030baaff
GustavoWillian7 <102532016+GustavoWillian7@users.noreply.github.com>
2022-04-03 15:13:11 -0300

    Add files via upload

---
+++ Exercicio1.java
@@ -0,0 +1,27 @@
+package Lista1;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+
+public class Exercicio1 {
+    public static void main(String args[]){
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.rint(Math.exp(1/2 * Math.log(64))));
+        System.out.println(Math.exp(1/2 * Math.log(64)));
+        System.out.println(Math.rint(Math.sin(45 * 3.14 / 180) + 0001.3));
+        System.out.println(Math.exp(3 * Math.log(8)) + 0001.3);
+        System.out.println(Math.rint(Math.exp(3 * Math.log(8))));
+        System.out.println(Math.exp(8 * 3.3));
+        System.out.println(Math.rint(Math.exp(8 * 3.3)));
+        System.out.println(Math.exp(1 / 3 * Math.log(8)));
+        System.out.println(Math.abs(-8));
+        System.out.println(Math.abs(8));
+        System.out.println(Math.rint(5.5));
+        System.out.println(Math.rint(6.5));
+        System.out.println(Math.rint(6.5 + 0.0001));
+        System.out.println(Math.rint(5.45));
+        System.out.println(Math.round(87));
+        System.out.println(Math.rint(3 / 4));
+    }
+}
